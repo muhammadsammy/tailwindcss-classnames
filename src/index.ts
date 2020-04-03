6fa27aa5df96d018598175703b0bd27ca98e5ff6
Krzysztof Żuraw <services@kzuraw.com>
2020-04-03 19:31:29 +0200

    feat: add css grid properties

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,8 @@ export type TDisplay =
   | 'inline'
   | 'flex'
   | 'inline-flex'
+  | 'grid'
+  | 'inline-grid'
   | 'table'
   | 'table-row'
   | 'table-cell'
@@ -564,6 +566,165 @@ export type TFlexBox =
   | TFlexGrow
   | TFlexShrink
   | TOrder;
+
+export type TGrid =
+  | TGridTemplateColumns
+  | TGridColumnStartEnd
+  | TGridTemplateRows
+  | TGridRowStartEnd
+  | TGridGap
+  | TGridAutoFlow;
+
+export type TGridTemplateColumns =
+  | 'grid-cols-1'
+  | 'grid-cols-2'
+  | 'grid-cols-3'
+  | 'grid-cols-4'
+  | 'grid-cols-5'
+  | 'grid-cols-6'
+  | 'grid-cols-7'
+  | 'grid-cols-8'
+  | 'grid-cols-9'
+  | 'grid-cols-10'
+  | 'grid-cols-11'
+  | 'grid-cols-12'
+  | 'grid-cols-none';
+
+export type TGridColumnStartEnd =
+  | 'col-auto'
+  | 'col-span-1'
+  | 'col-span-2'
+  | 'col-span-3'
+  | 'col-span-4'
+  | 'col-span-5'
+  | 'col-span-6'
+  | 'col-span-7'
+  | 'col-span-8'
+  | 'col-span-10'
+  | 'col-span-11'
+  | 'col-start-1'
+  | 'col-start-2'
+  | 'col-start-3'
+  | 'col-start-4'
+  | 'col-start-5'
+  | 'col-start-6'
+  | 'col-start-7'
+  | 'col-start-8'
+  | 'col-start-9'
+  | 'col-start-10'
+  | 'col-start-11'
+  | 'col-start-12'
+  | 'col-start-13'
+  | 'col-start-auto'
+  | 'col-end-1'
+  | 'col-end-2'
+  | 'col-end-3'
+  | 'col-end-4'
+  | 'col-end-5'
+  | 'col-end-6'
+  | 'col-end-7'
+  | 'col-end-8'
+  | 'col-end-9'
+  | 'col-end-10'
+  | 'col-end-11'
+  | 'col-end-12'
+  | 'col-end-13'
+  | 'col-end-auto';
+
+export type TGridTemplateRows =
+  | 'grid-rows-1'
+  | 'grid-rows-2'
+  | 'grid-rows-3'
+  | 'grid-rows-4'
+  | 'grid-rows-5'
+  | 'grid-rows-6'
+  | 'grid-rows-none';
+
+export type TGridRowStartEnd =
+  | 'row-auto'
+  | 'row-span-1'
+  | 'row-span-2'
+  | 'row-span-3'
+  | 'row-span-4'
+  | 'row-span-5'
+  | 'row-span-6'
+  | 'row-start-1'
+  | 'row-start-2'
+  | 'row-start-3'
+  | 'row-start-4'
+  | 'row-start-5'
+  | 'row-start-6'
+  | 'row-start-7'
+  | 'row-start-auto'
+  | 'row-end-1'
+  | 'row-end-2'
+  | 'row-end-3'
+  | 'row-end-4'
+  | 'row-end-5'
+  | 'row-end-6'
+  | 'row-end-7'
+  | 'row-end-auto';
+
+export type TGridGap =
+  | 'gap-0'
+  | 'gap-1'
+  | 'gap-2'
+  | 'gap-3'
+  | 'gap-4'
+  | 'gap-5'
+  | 'gap-6'
+  | 'gap-8'
+  | 'gap-10'
+  | 'gap-12'
+  | 'gap-16'
+  | 'gap-20'
+  | 'gap-24'
+  | 'gap-32'
+  | 'gap-40'
+  | 'gap-48'
+  | 'gap-56'
+  | 'gap-64'
+  | 'gap-px'
+  | 'row-gap-0'
+  | 'row-gap-1'
+  | 'row-gap-2'
+  | 'row-gap-3'
+  | 'row-gap-4'
+  | 'row-gap-5'
+  | 'row-gap-6'
+  | 'row-gap-8'
+  | 'row-gap-10'
+  | 'row-gap-12'
+  | 'row-gap-16'
+  | 'row-gap-20'
+  | 'row-gap-24'
+  | 'row-gap-32'
+  | 'row-gap-40'
+  | 'row-gap-48'
+  | 'row-gap-56'
+  | 'row-gap-64'
+  | 'row-gap-px'
+  | 'col-gap-0'
+  | 'col-gap-1'
+  | 'col-gap-2'
+  | 'col-gap-3'
+  | 'col-gap-4'
+  | 'col-gap-5'
+  | 'col-gap-6'
+  | 'col-gap-8'
+  | 'col-gap-10'
+  | 'col-gap-12'
+  | 'col-gap-16'
+  | 'col-gap-20'
+  | 'col-gap-24'
+  | 'col-gap-32'
+  | 'col-gap-40'
+  | 'col-gap-48'
+  | 'col-gap-56'
+  | 'col-gap-64'
+  | 'col-gap-px';
+
+export type TGridAutoFlow = 'grid-flow-row' | 'grid-flow-col' | 'grid-flow-row-dense' | 'grid-flow-col-dense';
 
 export type TPadding =
   | 'p-0'
@@ -1131,6 +1292,7 @@ export type TClasses =
   | TBackgrounds
   | TBorders
   | TFlexBox
+  | TGrid
   | TSpacing
   | TSizing
   | TTables
