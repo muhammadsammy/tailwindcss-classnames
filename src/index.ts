5bd51a0b12377d1fe5a0d6fdc101922aa9dfa92c
Muhammad Sammy <themsammy@outlook.com>
2020-04-10 16:29:02 +0200

    Add missing line height classes

--- src/index.ts
+++ src/index.ts
@@ -115,7 +115,15 @@ export type TLineHeight =
   | 'leading-snug'
   | 'leading-normal'
   | 'leading-relaxed'
-  | 'leading-loose';
+  | 'leading-loose'
+  | 'leading-3'
+  | 'leading-4'
+  | 'leading-5'
+  | 'leading-6'
+  | 'leading-7'
+  | 'leading-8'
+  | 'leading-9'
+  | 'leading-10';
 
 export type TListStyleType = 'line-none' | 'list-disc' | 'list-decimal';
 
