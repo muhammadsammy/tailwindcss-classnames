d86b25d5d0bef96dec6b0cf5c448088457ab014b
Muhammad Sammy <themsammy@outlook.com>
2020-04-14 22:02:03 +0200

    Add scale classes

--- src/index.ts
+++ src/index.ts
@@ -1444,6 +1444,40 @@ export type TTransitionTimingFunction = 'ease-linear' | 'ease-in' | 'ease-out' |
 
 export type TTransitions = TTransitionProperty | TTransitionDuration | TTransitionTimingFunction;
 
+export type TScale =
+  | 'scale-0'
+  | 'scale-50'
+  | 'scale-75'
+  | 'scale-90'
+  | 'scale-95'
+  | 'scale-100'
+  | 'scale-105'
+  | 'scale-110'
+  | 'scale-125'
+  | 'scale-150'
+  | 'scale-x-0'
+  | 'scale-x-50'
+  | 'scale-x-75'
+  | 'scale-x-90'
+  | 'scale-x-95'
+  | 'scale-x-100'
+  | 'scale-x-105'
+  | 'scale-x-110'
+  | 'scale-x-125'
+  | 'scale-x-150'
+  | 'scale-y-0'
+  | 'scale-y-50'
+  | 'scale-y-75'
+  | 'scale-y-90'
+  | 'scale-y-95'
+  | 'scale-y-100'
+  | 'scale-y-105'
+  | 'scale-y-110'
+  | 'scale-y-125'
+  | 'scale-y-150';
+
+export type TTransforms = TScale;
+
 export type TAppearance = 'appearance-none';
 
 export type TCursor =
