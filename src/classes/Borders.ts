0485f48cb7ecf1f4aa54b1e5dc49b32b7af7f5f7
Muhammad Sammy <themsammy@outlook.com>
2020-05-18 18:25:54 +0200

    Add missing devideWidth in Borders.ts

--- src/classes/Borders.ts
+++ src/classes/Borders.ts
@@ -183,9 +183,25 @@ const borderRadius = [
   'rounded-bl-full',
 ];
 
+const devideWidth = [
+  'divide-x-0',
+  'divide-x-2',
+  'divide-x-4',
+  'divide-x-8',
+  'divide-x',
+  'divide-y-0',
+  'divide-y-2',
+  'divide-y-4',
+  'divide-y-8',
+  'divide-y',
+  'divide-x-reverse',
+  'divide-y-reverse',
+];
+
 export const Borders = {
   borderColor,
   borderStyle,
   borderWidth,
   borderRadius,
+  devideWidth,
 };
