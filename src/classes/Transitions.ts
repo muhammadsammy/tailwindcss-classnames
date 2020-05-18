f0e4ebf7bf95a7b9b479839d592b6e87aa22338c
Muhammad Sammy <themsammy@outlook.com>
2020-05-18 03:57:39 +0200

    Add transition delays to transitions classes in classes/Transition.ts

--- src/classes/Transitions.ts
+++ src/classes/Transitions.ts
@@ -19,10 +19,22 @@ const transitionDuration = [
   'duration-1000',
 ];
 
+const transitionDelay = [
+  'delay-75',
+  'delay-100',
+  'delay-150',
+  'delay-200',
+  'delay-300',
+  'delay-500',
+  'delay-700',
+  'delay-1000',
+];
+
 const transitionTimingFunction = ['ease-linear', 'ease-in', 'ease-out', 'ease-in-out'];
 
 export const Transitions = {
   transitionProperty,
   transitionDuration,
   transitionTimingFunction,
+  transitionDelay,
 };
