0d3dd4743c1796c359e41b8dfdfeb0845c890157
Muhammad Sammy <themsammy@outlook.com>
2020-05-30 23:26:54 +0200

    Refactor and add types to ConfigScanner.ts

--- src/generation/ConfigScanner.ts
+++ src/generation/ConfigScanner.ts
@@ -1,47 +1,51 @@
-import isEmpty from 'lodash.isempty';
-import { defaultColors, defaultScreens, defaultSpacing, defaultVariants, defaultOpacities } from './utils/utils';
+import { TailwindConfig, IThemeConfig, IVariantsConfig } from './TailwindConfigTypes';
+import { defaultThemeConfig, defaultVariants } from './utils/defaultTailwindConfig';
+
+type ColorShades = string[] | Array<{ [key: string]: string }>;
 
 export class ConfigScanner {
-  public readonly prefix: any;
-  public readonly separator: any;
-  public readonly themeConfig: any;
-  private readonly variantsConfig: any;
+  public readonly prefix: string;
+  public readonly separator: string;
+  public readonly themeConfig: IThemeConfig;
+  private readonly variantsConfig: IVariantsConfig;
 
-  constructor(tailwindConfig: any) {
-    this.variantsConfig = tailwindConfig?.variants;
-    this.themeConfig = tailwindConfig?.theme;
-    this.prefix = isEmpty(tailwindConfig.prefix) ? '' : tailwindConfig.prefix;
-    this.separator = isEmpty(tailwindConfig.separator) ? ':' : tailwindConfig.separator;
+  constructor(tailwindConfig: TailwindConfig) {
+    this.prefix = tailwindConfig.prefix ?? '';
+    this.separator = tailwindConfig.separator ?? ':';
+    this.variantsConfig = tailwindConfig.variants ?? defaultVariants;
+    this.themeConfig = tailwindConfig.theme ?? (defaultThemeConfig as IThemeConfig);
   }
 
-  public getThemeColors = () => {
-    const themeColors = isEmpty(this.themeConfig?.colors) ? defaultColors : this.themeConfig?.colors;
+  public getThemeColors = (): { colorsNames: string[]; colorsShades: ColorShades } => {
+    const themeColors = this.themeConfig?.colors ?? defaultThemeConfig.colors;
     const extendedThemeColors = this.themeConfig?.extend?.colors;
     const allConfigColors = extendedThemeColors ? { ...themeColors, ...extendedThemeColors } : themeColors;
-    type ColorShades = string[] | Array<{ [key: string]: string }>;
+
     return {
       colorsNames: Object.keys(allConfigColors),
       colorsShades: Object.values(allConfigColors) as ColorShades,
     };
   };
 
-  public getThemeBreakpoints = () => {
-    const themeBreakpoints = isEmpty(this.themeConfig?.screens) ? defaultScreens : this.themeConfig?.screens;
+  public getThemeBreakpoints = (): string[] => {
+    const themeBreakpoints = this.themeConfig?.screens ?? defaultThemeConfig.screens;
     const extendedThemeBreakpoints = this.themeConfig?.extend?.screens;
     const allConfigBreakpoints = extendedThemeBreakpoints
       ? { ...themeBreakpoints, ...extendedThemeBreakpoints }
       : themeBreakpoints;
+
     return Object.keys(allConfigBreakpoints);
   };
 
-  public getThemeOpacities = () => {
-    const themeOpacities = isEmpty(this.themeConfig?.opacity) ? defaultOpacities : this.themeConfig?.opacity;
+  public getThemeOpacities = (): { [key: string]: string } => {
+    const themeOpacities = this.themeConfig?.opacity ?? defaultThemeConfig.opacity;
     const extendedThemeOpacities = this.themeConfig?.extend?.opacity;
+
     return extendedThemeOpacities ? { ...themeOpacities, ...extendedThemeOpacities } : themeOpacities;
   };
 
-  public getThemeSpacing = () => {
-    const themeSpacing = isEmpty(this.themeConfig?.spacing) ? defaultSpacing : this.themeConfig?.spacing;
+  public getThemeSpacing = (): { spacingKeys: string[]; spacingValues: string[] } => {
+    const themeSpacing = this.themeConfig?.spacing ?? defaultThemeConfig.spacing;
     const extendedThemeSpacing = this.themeConfig?.extend?.spacing;
     const allConfigSpacing = extendedThemeSpacing ? { ...themeSpacing, ...extendedThemeSpacing } : themeSpacing;
 
@@ -51,8 +55,8 @@ export class ConfigScanner {
     };
   };
 
-  public getPseudoClassVariants = () => {
-    const themeVariants = isEmpty(this.variantsConfig) ? defaultVariants : this.variantsConfig;
+  public getPseudoClassVariants = (): { classesCategories: string[]; classesVariants: string[][] } => {
+    const themeVariants = this.variantsConfig;
     Object.keys(themeVariants).map(key => {
       if (Object.keys(defaultVariants).includes(key)) {
         delete defaultVariants[key];
