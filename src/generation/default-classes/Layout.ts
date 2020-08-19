1c3e5e07776c138886e2dd19449f496f30597a29
muhammadsammy <themsammy@outlook.com>
2020-08-20 00:10:13 +0200

    feat: add overscroll-behavior classes types

--- src/generation/default-classes/Layout.ts
+++ src/generation/default-classes/Layout.ts
@@ -57,6 +57,18 @@ const overflow = [
   'scrolling-auto',
 ];
 
+const overscrollBehavior = [
+  'overscroll-auto',
+  'overscroll-contain',
+  'overscroll-none',
+  'overscroll-y-auto',
+  'overscroll-y-contain',
+  'overscroll-y-none',
+  'overscroll-x-auto',
+  'overscroll-x-contain',
+  'overscroll-x-none',
+];
+
 const position = ['static', 'fixed', 'absolute', 'relative', 'sticky'];
 
 const topRightBottomLeft = [
@@ -89,6 +101,7 @@ export const Layout = {
   objectFit,
   objectPosition,
   overflow,
+  overscrollBehavior,
   position,
   topRightBottomLeft,
   visibility,
