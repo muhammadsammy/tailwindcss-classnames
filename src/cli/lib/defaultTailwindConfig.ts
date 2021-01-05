92770f37eb5ecf4f2409c8d6e819bcb89e8864bc
Muhammad Sammy <muhammad_sammy@outlook.com>
2021-01-05 13:25:18 +0200

    feat: update default config with new breakpoint scales

--- src/cli/lib/defaultTailwindConfig.ts
+++ src/cli/lib/defaultTailwindConfig.ts
@@ -16,6 +16,7 @@ export const defaultTailwindConfig = {
       md: '768px',
       lg: '1024px',
       xl: '1280px',
+      '2xl': '1536px',
     },
     colors: {
       transparent: 'transparent',
